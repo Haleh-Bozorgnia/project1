d6b69cdf92758d46301d1d61303e8b0b76106d2c
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-05-27 12:53:54 -0700

    start to design

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ $searchForm.on("submit", (event) => {
   const bookTitle = formData.get("bookTitle").toLocaleLowerCase();
 
   const $screen = $(".screen");
-  // const $result = $("#result");
 
   //empty out the input field
   // $('[name="bookTitle"]')[0].value = "";
@@ -21,82 +20,46 @@ $searchForm.on("submit", (event) => {
   const url = `https://openlibrary.org/search.json?title=${bookTitle}`;
 
   $.ajax(url).then((response) => {
+    //print the output of URL
     console.log(response);
-    const $resultDiv = $(".result");
-
     console.log(response.docs);
-    let arr = response.docs;
 
+    //define varaible to show the output
+    const $resultDiv = $(".result");
+    //getting the API output as an array
+    let arr = response.docs;
+    //loop over the respone array
     for (let i = 0; i < arr.length; i++) {
+      //print all the neccessory field
       console.log(arr[i].title);
       const div = $("<div>");
-
       console.log(arr[i].author_name);
       console.log(arr[i].first_publish_year);
       console.log(arr[i].ebook_access);
       console.log(arr[i].author_key[0]);
+      //define the variable to access the author key
 
-      div.html(`<h5>Book Name: ${arr[i].title}</h5>
-                <h5>Author Name: ${arr[i].author_name}</h5>
-                <h5>First pblish Year: ${arr[i].first_publish_year}</h5>
-                <h5>Ebook Access: ${arr[i].ebook_access}</h>         
-      `);
-      $resultDiv.append(div);
       const imagename = arr[i].author_key[0];
+      //define the variable fir image of author
       const img = $("<img />", {
-        src: `https://covers.openlibrary.org/a/olid/${imagename}-S.jpg`,
+        src: `https://covers.openlibrary.org/a/olid/${imagename}-M.jpg`,
       });
-      img.appendTo(".screen");
+      //show and append the result on screen
+      // div.html(`<h5>Book Name: ${arr[i].title}</h5>
+      //           <h5>Author Name: ${arr[i].author_name}</h5>
+      //           <h5>First pblish Year: ${arr[i].first_publish_year}</h5>
+      //           <h5>Ebook Access: ${arr[i].ebook_access}</h> 
+             
+      // `);
+        div.html(`<div>Book Name: ${arr[i].title}</div>
+                <div>Author Name: ${arr[i].author_name}</div>
+                <div>First pblish Year: ${arr[i].first_publish_year}</div>
+                <div>Ebook Access: ${arr[i].ebook_access}</div> 
+                <br>             
+      `);
+
+      $resultDiv.append(div, img);
+
     }
-
-    // console.log(response);
-
-    //   $result.html(`
-    //   <div>
-    //     title 1:${docs.title}
-    //   </div>
-    //   <div>
-    //     Author Name: ${docs.author_name}
-    //   </div>
-    //   <div>
-    //    First Publish Year: ${docs.first_publish_year}
-    //   <div
-    //   <div>
-    //     Ebook Access: ${docs.ebook_access}
-    //   </div>
-    // `);
   });
-
-  //===============================================
-  //===============================================
-  // https://covers.openlibrary.org/a/olid/OL229501A-S.jpg
-
-  //  $.ajax(url).then((response) => {
-  //     console.log(response);
-  //     const $resultDiv = $(".result");
-
-  //     // const doc = response.docs.map((d) => {
-  //     //   return { title: d.title};
-  //     // });
-  //     // const div = $("<div>");
-
-  //     console.log(response.docs);
-  //     let arr2 = response.docs;
-  //     for (let i = 0; i < arr2.length; i++) {
-  //       console.log(arr[i].title);
-  //       const div = $("<div>");
-
-  //       console.log(arr[i].author_name);
-  //       console.log(arr[i].first_publish_year);
-  //       console.log(arr[i].ebook_access);
-  //       const pic = "https://covers.openlibrary.org/b/olid/OL7440033M-S.jpg";
-
-  //       div.html(`<h5>Book Name: ${arr[i].title}</h5>
-  //                 <h5>Author Name: ${arr[i].author_name}</h5>
-  //                 <h5>First pblish Year: ${arr[i].first_publish_year}</h5>
-  //                 <h5>Ebook Access: ${arr[i].ebook_access}</h5
-  //                 <img src=${pic}/>
-  //       `);
-  //       $resultDiv.append(div);
-  //     }
 });
