23200e54c3de3c53e08ee1c7898aebd71c5d0b80
Haleh Bozorgnia <bozorgnia2010@gmail.com>
2023-05-27 10:25:03 -0700

    work on getting the author pictures

--- app.js
+++ app.js
@@ -24,13 +24,9 @@ $searchForm.on("submit", (event) => {
     console.log(response);
     const $resultDiv = $(".result");
 
-    // const doc = response.docs.map((d) => {
-    //   return { title: d.title};
-    // });
-    // const div = $("<div>");
-
     console.log(response.docs);
     let arr = response.docs;
+
     for (let i = 0; i < arr.length; i++) {
       console.log(arr[i].title);
       const div = $("<div>");
@@ -38,13 +34,19 @@ $searchForm.on("submit", (event) => {
       console.log(arr[i].author_name);
       console.log(arr[i].first_publish_year);
       console.log(arr[i].ebook_access);
+      console.log(arr[i].author_key[0]);
 
       div.html(`<h5>Book Name: ${arr[i].title}</h5>
                 <h5>Author Name: ${arr[i].author_name}</h5>
                 <h5>First pblish Year: ${arr[i].first_publish_year}</h5>
-                <h5>Ebook Access: ${arr[i].ebook_access}</h5
+                <h5>Ebook Access: ${arr[i].ebook_access}</h>         
       `);
       $resultDiv.append(div);
+      const imagename = arr[i].author_key[0];
+      const img = $("<img />", {
+        src: `https://covers.openlibrary.org/a/olid/${imagename}-S.jpg`,
+      });
+      img.appendTo(".screen");
     }
 
     // console.log(response);
@@ -64,4 +66,37 @@ $searchForm.on("submit", (event) => {
     //   </div>
     // `);
   });
+
+  //===============================================
+  //===============================================
+  // https://covers.openlibrary.org/a/olid/OL229501A-S.jpg
+
+  //  $.ajax(url).then((response) => {
+  //     console.log(response);
+  //     const $resultDiv = $(".result");
+
+  //     // const doc = response.docs.map((d) => {
+  //     //   return { title: d.title};
+  //     // });
+  //     // const div = $("<div>");
+
+  //     console.log(response.docs);
+  //     let arr2 = response.docs;
+  //     for (let i = 0; i < arr2.length; i++) {
+  //       console.log(arr[i].title);
+  //       const div = $("<div>");
+
+  //       console.log(arr[i].author_name);
+  //       console.log(arr[i].first_publish_year);
+  //       console.log(arr[i].ebook_access);
+  //       const pic = "https://covers.openlibrary.org/b/olid/OL7440033M-S.jpg";
+
+  //       div.html(`<h5>Book Name: ${arr[i].title}</h5>
+  //                 <h5>Author Name: ${arr[i].author_name}</h5>
+  //                 <h5>First pblish Year: ${arr[i].first_publish_year}</h5>
+  //                 <h5>Ebook Access: ${arr[i].ebook_access}</h5
+  //                 <img src=${pic}/>
+  //       `);
+  //       $resultDiv.append(div);
+  //     }
 });
